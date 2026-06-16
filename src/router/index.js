66c70c394745fe652e01e912f00f6a1af169c935
vtian <1074025714@qq.com>
2026-06-16 23:50:21 +0800

    Configure GitHub Pages deployment

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import HotMoneySeatsView from '../views/HotMoneySeatsView.vue'
 import DailyBillboardView from '../views/DailyBillboardView.vue'
@@ -37,7 +37,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior() {
     return { top: 0 }
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import UnoCSS from 'unocss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/a8/',
   plugins: [vue(), UnoCSS()],
   server: {
     proxy: {
